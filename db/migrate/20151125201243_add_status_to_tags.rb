5684f58d95b84337faf5cca6baca29ab6f052f6a
telwell <me@trevorelwell.com>
2015-11-26 14:57:49 -0500

    fixing migration error

--- db/migrate/20151125201243_add_status_to_tags.rb
+++ db/migrate/20151125201243_add_status_to_tags.rb
@@ -1,6 +1,5 @@
 class AddStatusToTags < ActiveRecord::Migration
   def change
-  	remove_column :tags, :characters, :string
   	remove_column :tags, :character, :string
   	add_column :tags, :status, :boolean, default: false
   end
